d7f7330f668f7529a1c7e09411b10f8458e914e5
Rush <itzashert@gmail.com>
2025-02-04 21:55:16 +0530

    1:25:43 ended so far and going to start with FireBase and Stuff

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useAuth } from "@clerk/clerk-react";
+import { cn } from "@/lib/utils";
+import { Container } from "./container";
+import { LogoContainer } from "./logo-container";
+import { NavigationRoutes } from "./ui/navigation-routes";
+import { NavLink } from "react-router-dom";
+import {ProfileContainer} from "./profile-container";
+import {ToggleContainer} from "./toggle-container";
 
-function Header() {
+const Header = () => {
+  const { userId } = useAuth();
+
   return (
-    <div>Header</div>
-  )
-}
+    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+      <Container>
+        <div className="flex items-center gap-6 w-full">
+          {/* Logo Section */}
+          <LogoContainer />
 
-export default Header
+          {/* Navigation Section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink
+                to="/generate"
+                className={({ isActive }) =>
+                  cn("text-base text-neutral-600", isActive && "text-neutral-900 font-semibold")
+                }
+              >
+                Take an Interview
+              </NavLink>
+            )}
+          </nav>
+
+          <div className="ml-auto flex items-center gap-6">
+          {/* Profile Section */}
+          <ProfileContainer/>
+
+          {/*Mobile Toggle Section  */}
+          <ToggleContainer/>
+
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
